a6f912e1b1e52591c75bef141c05d52f736b028d
Hemantmishra-lab <hemantmishra19781978@gmail.com>
2026-04-30 17:57:22 +0530

    Greedy Method Approach

--- src/GreedyApproach/FractionalKnapsack.java
+++ src/GreedyApproach/FractionalKnapsack.java
@@ -1,6 +1,7 @@
 package GreedyApproach;
 //
 import java.util.Arrays;
+import java.util.Scanner;
 //
 //class Item{
 //    int value,weight;
@@ -44,4 +45,54 @@ import java.util.Arrays;
 //    }
 //}
 
+class Items{
+    int value;
+    int weight;
+    double ratio;
+    public Items(int value,int weight){
+        this.value = value;
+        this.weight  = weight;
+        this.ratio = (double) value / (double)weight;
+    }
+}
+public class FractionalKnapsack{
 
+    public static double MaxValue(int[]value,int[] weight,int capacity){
+        int n= value.length;
+        Items []items = new Items[n];
+
+        for(int i=0;i<n;i++){
+            items[i] = new Items(value[i],weight[i]);
+        }
+        Arrays.sort(items,(a,b)->Double.compare(a.ratio , b.ratio));
+        double MaxMoney = 0.0;
+        for(int i=0;i<n;i++){
+            Items currentItem = items[i];
+            if(capacity -  currentItem.weight >= 0 ){
+                capacity = capacity - currentItem.weight;
+                MaxMoney = MaxMoney + currentItem.value;
+            }
+            else{
+                double fraction = (double) capacity / (double) currentItem.weight;
+                MaxMoney = MaxMoney + (fraction * currentItem.value);
+                break;
+            }
+        }
+        return MaxMoney;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []value = new int[n];
+        int[] weight = new int[n];
+        System.out.println("Enter the Capacity For the Bag : ");
+        int capacity = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.print("Value "+i);
+            value[i]=sc.nextInt();
+            System.out.println("Weight "+i);
+            weight[i] = sc.nextInt();
+        }
+        System.out.println(MaxValue(value,weight,capacity));
+    }
+}
