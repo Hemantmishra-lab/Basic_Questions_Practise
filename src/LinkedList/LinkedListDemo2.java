e54d8b6e3c8ec73b15ae154a0d912ee9999be1f5
Hemantmishra-lab <hemantmishra19781978@gmail.com>
2026-06-18 19:37:34 +0530

    Revision

--- src/LinkedList/LinkedListDemo2.java
+++ src/LinkedList/LinkedListDemo2.java
@@ -95,6 +95,145 @@ import java.util.LinkedList;
 //    }
 //}
 
+//class Node{
+//    int data;
+//    Node next;
+//    public Node(int data){
+//        this.data = data;
+//        this.next = null;
+//    }
+//}
+//
+//public class LinkedListDemo2 {
+//    public static Node head = null;
+//    public static void addBeg(int data){
+//        Node newNode = new Node(data);
+//        newNode.next = head;
+//        head = newNode;
+//    }
+//    public static void addLast(int data){
+//        Node newNode = new Node(data);
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//        temp.next  = newNode;
+//    }
+//    public static Node LinkedListReverse(){
+//        Node curr = head;
+//        Node prev = null;
+//        while(curr!=null){
+//            Node nextNode = curr.next;
+//            curr.next = prev;
+//            prev = curr;
+//            curr = nextNode;
+//        }
+//        return prev;
+//    }
+//    public static void display(){
+//        Node temp = head;
+//        while(temp!=null){
+//            System.out.print(temp.data+" ->");
+//            temp = temp.next;
+//        }
+//        System.out.println("null");
+//    }
+//    public static void main(String[] args){
+//        addBeg(1);
+//        addBeg(2);
+//        addLast(3);
+//        addLast(4);
+//        addBeg(5);
+//        display();
+//        Node head = LinkedListReverse();
+//        display();
+//    }
+//}
+
+//
+//class Node{
+//    int data;
+//    Node next;
+//    public Node(int data){
+//        this.data = data;
+//        this.next = null;
+//    }
+//}
+//public class LinkedListDemo2 {
+//    public static Node head = null;
+//
+//    public static void addBeg(int data){
+//        Node newNode = new Node(data);
+//        newNode.next = head;
+//        head = newNode;
+//    }
+//
+//    public static void addLast(int data){
+//        Node newNode= new Node(data);
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//        temp.next = newNode;
+//    }
+//
+//    public static int MiddleElement(){
+//        Node fast = head;Node slow = head;
+//        while(fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//        return slow.data;
+//    }
+//
+//    public static void revLinkedList(){
+//        Node prev = null;
+//        Node curr = head;
+//        while(curr != null){
+//            Node nextNode = curr.next;
+//            curr.next = prev;
+//            prev = curr;
+//            curr = nextNode;
+//        }
+//        head = prev;
+//    }
+//
+//    public static boolean isCycle(){
+//        Node fast = head;Node slow = head;
+//        while(fast.next != null){
+//            if(slow.next == fast.next.next){
+//                return true;
+//            }
+//            fast = fast.next.next;
+//        }
+//        return false;
+//    }
+//
+//    public static void display(){
+//        Node temp = head;
+//        while(temp != null){
+//            System.out.print(temp.data+" ->");
+//            temp = temp.next;
+//        }
+//        System.out.print("null");
+//        System.out.println();
+//    }
+//
+//    public static void main(String[] args){
+//        addBeg(1);
+//        addLast(2);
+//        addLast(3);
+//        addLast(4);
+//        addLast(5);
+//        revLinkedList();
+//        display();
+//        System.out.println(MiddleElement());
+//        System.out.println(isCycle());
+//    }
+//}
+//
+
+
 class Node{
     int data;
     Node next;
@@ -103,10 +242,10 @@ class Node{
         this.next = null;
     }
 }
-
 public class LinkedListDemo2 {
     public static Node head = null;
-    public static void addBeg(int data){
+
+    public static void addBed(int data){
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
@@ -117,41 +256,104 @@ public class LinkedListDemo2 {
         while(temp.next != null){
             temp = temp.next;
         }
-        temp.next  = newNode;
+        temp.next = newNode;
     }
-    public static Node LinkedListReverse(){
+    public static void addIndex(int index,int data){
+        Node newNode = new Node(data);
+        Node temp = head;
+        for(int i=1;i<index-1;i++){
+            temp =  temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    public static boolean search(int data){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == data)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+    public static void delete(int index){
+        Node temp = head;
+        for(int i=1;i<=index-1;i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+    public static void reverseLinkedList(){
         Node curr = head;
         Node prev = null;
-        while(curr!=null){
-            Node nextNode = curr.next;
+        while(curr != null){
+            Node newNode = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = nextNode;
+            curr = newNode;
         }
-        return prev;
+        head = prev;
+    }
+    public static int middleNode(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+    public static int nthEnd(int k){
+        Node first = head;
+        Node second = head;
+        for(int i=1;i<=k;i++){
+            first = first.next;
+        }
+        while(first != null){
+            second = second.next;
+            first = first.next;
+        }
+        return second.data;
+    }
+    public static boolean cycleDetection(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
     }
     public static void display(){
         Node temp = head;
-        while(temp!=null){
+        while(temp != null){
             System.out.print(temp.data+" ->");
             temp = temp.next;
         }
-        System.out.println("null");
+        System.out.print("null");
+        System.out.println();
     }
     public static void main(String[] args){
-        addBeg(1);
-        addBeg(2);
+        addBed(1);
+        addLast(2);
         addLast(3);
+        addIndex(2,99);
+        delete(2);
+        addLast(5);
         addLast(4);
-        addBeg(5);
+        addLast(5);
+        System.out.println("Search "+search(5));
+        System.out.println(cycleDetection());
         display();
-        Node head = LinkedListReverse();
+        System.out.println(nthEnd(2));
+        System.out.println(middleNode());
+        reverseLinkedList();
         display();
     }
 }
-
-
-
 
 
 
